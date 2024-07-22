87f57e75215218cc463fffc8cfe234cde49cacb9
Namgyu Ho <itsnamgyu@gmail.com>
2024-07-22 16:10:09 +0900

    Review on July 22nd

--- metric_tracker.py
+++ metric_tracker.py
@@ -1,42 +1,60 @@
+from typing import List
+
 import numpy as np
-import numpy.ma as ma
 import omegaconf
-from typing import Set, Dict, List
 import pandas as pd
-
-from sklearn.metrics import mean_squared_error
 from scipy.stats import pearsonr
-
-from util.time.timestamp import Timestamp
+from sklearn.metrics import mean_squared_error
 
 from metrics import calculate_confusion_matrix
+from util.time.timestamp import Timestamp
+
 
 class DictConfig(omegaconf.DictConfig):
-    n_regions: int
-
+    """
+    Class to inform arguments needed for MetricTracker.
+    """
+    evaluation_thresholds: List[float]
 
 
 class MetricTracker:
+    """
+    Class to track and save metrics for regional and grid predictions.
+
+    Attributes:
+        evaluation_thresholds: list of thresholds for classification
+        binary_classification_index: index of binary threshold for binary classification metrics
+        regional_df: raw data for regional predictions.
+        grid_df: statistics (confusion matrix) for grid predictions aggregated over timestamps and regions.
+    """
+
     def __init__(
         self,
         args: DictConfig
     ) -> None:
-        # n_regression_metrics = 4
-        self.evaluation_thresholds = args.evaluation_threhsolds
-        self.evaluation_boundary = 2 # index of value 나쁨 # TODO
+        self.evaluation_thresholds = args.evaluation_thresholds
+        self.binary_classification_index = 2  # index of value 나쁨 # TODO  # NAMGYU: do what?
 
-        self.regional_df = pd.DataFrame(columns=['date', 'leadtime', 'region', 'target_cont', 'predict_cont', 'target_cate', 'predict_cate'])
-        self.grid_df = pd.DataFrame(columns=['date', 'leadtime', 'region', 'g0p0', 'g0p1', 'g0p2', 'g0p3', 'g1p0', 'g1p1', 'g1p2', 'g1p3', 'g2p0', 'g2p1', 'g2p2', 'g2p3', 'g3p0', 'g3p1', 'g3p2', 'g3p3'])
+        self.regional_df = pd.DataFrame(
+            columns=['origin', 'lead_time', 'region', 'target', 'prediction', 'categorical_target',
+                     'categorical_prediction'])
+        self.grid_df = pd.DataFrame(
+            columns=['origin', 'lead_time', 'region', 'g0p0', 'g0p1', 'g0p2', 'g0p3', 'g1p0', 'g1p1', 'g1p2', 'g1p3',
+                     'g2p0', 'g2p1', 'g2p2', 'g2p3', 'g3p0', 'g3p1', 'g3p2', 'g3p3'])
 
         self.device = args.device
 
-    def add_regional_predictions(self, timestamp: List[Timestamp], prediction: np.array, target: np.array):  # continuous input -> regression, classification
+    def add_regional_predictions(self, timestamp: List[Timestamp], prediction: np.array,
+                                 target: np.array):
         """
-        Add raw regional predictions to self.regional_df
+        Update self.regional_df with new batch of regional predictions (and targets), given in continuous values.
 
-        :param timestamp: list of timestamps
-        :param prediction: np.array of shape (B, N)
-        :param target: np.array of shape (B, N)
+        Args:
+            timestamp: list of timestamps
+            prediction: np.array of shape (B, N) with continuous values
+            target: np.array of shape (B, N) with continuous values
+
+        Returns:
         """
 
         categorical_prediction = np.digitize(prediction, bins=self.evaluation_thresholds)
@@ -44,26 +62,27 @@ class MetricTracker:
 
         # save into dataframe
         b = len(timestamp)
-        dates = [t.origin for t in timestamp for _ in range(19)]
+        origins = [t.origin for t in timestamp for _ in range(19)]
         lead_times = [t.lead_time for t in timestamp for _ in range(19)]
         regions = list(range(19)) * b
 
         new_data = pd.DataFrame({
-            'date': dates,
-            'leadtime': lead_times,
-            'region': regions, 
-            'target_cont': target.flatten().tolist(),
-            'predict_cont': prediction.flatten().tolist(),
-            'target_cate': categorical_target.flatten().tolist(),
-            'predict_cate': categorical_prediction.flatten().tolist()
+            'origin': origins,
+            'lead_time': lead_times,
+            'region': regions,
+            'target': target.flatten().tolist(),
+            'prediction': prediction.flatten().tolist(),
+            'categorical_target': categorical_target.flatten().tolist(),
+            'categorical_prediction': categorical_prediction.flatten().tolist()
         })
         self.regional_df = pd.concat([self.regional_df, new_data], ignore_index=True)
 
-    def add_grid_predictions(self, timestamp: List[Timestamp], prediction: np.array, target: np.array):  # continuous input -> classification
+    def add_grid_predictions(self, timestamp: List[Timestamp], prediction: np.array,
+                             target: np.array) -> None:
         """
-        Add confusion matrix of grid predictions to self.grid_df
+        Update self.grid_df with new batch of grid predictions (and targets), given in continuous values.
 
-        :param timestamp: list of timestamps
+        :param timestamp: list of B timestamps
         :param prediction: np.array of shape (B, W, H)
         :param target: np.array of shape (B, W, H)
         """
@@ -75,62 +94,59 @@ class MetricTracker:
         categorical_target = np.digitize(target, bins=self.evaluation_thresholds)
 
         # load regional masks into shape of (19, -1)
-        masks = np.random.randint(2, size=(19, 83, 67)) # TODO GET_MASK_HERE # (M, W, H) # TODO -1 for missing value
+        masks = np.random.randint(2, size=(19, 83, 67))  # TODO GET_MASK_HERE # (M, W, H) # TODO -1 for missing value
         masks = masks.reshape(19, -1)
 
         # split the prediction and target into 19 regions
-        categorical_prediction = np.repeat(categorical_prediction[:, np.newaxis, :], 19, axis=1) # (B, R, W*H)
-        categorical_target = categorical_target[:, np.newaxis, :] * masks[np.newaxis, :, :] # (B, R, W*H) 
+        categorical_prediction = np.repeat(categorical_prediction[:, np.newaxis, :], 19, axis=1)  # (B, R, W*H)
+        categorical_target = categorical_target[:, np.newaxis, :] * masks[np.newaxis, :, :]  # (B, R, W*H)
 
         # calculate confusion matrix    
-        classification_output = calculate_confusion_matrix(categorical_prediction.reshape(b*19, -1), categorical_target.reshape(b*19, -1),
-                                                           n_category=len(self.evaluation_thresholds)+1, device=self.device) # (B*R, 4*4)
+        classification_output = calculate_confusion_matrix(categorical_prediction.reshape(b * 19, -1),
+                                                           categorical_target.reshape(b * 19, -1),
+                                                           n_classes=len(self.evaluation_thresholds) + 1,
+                                                           device=self.device)  # (B*R, 4*4)
 
         # save into dataframe
-        dates = [t.origin for t in timestamp for _ in range(19)]
+        origins = [t.origin for t in timestamp for _ in range(19)]
         lead_times = [t.lead_time for t in timestamp for _ in range(19)]
         regions = list(range(19)) * b
 
         classification_output = classification_output.reshape(b, 19, 4, 4)
 
         new_data = pd.DataFrame({
-                'date': dates,
-                'leadtime': lead_times,
-                'region': regions,
-                'g0p0': classification_output[:, :, 0, 0].flatten().tolist(),
-                'g0p1': classification_output[:, :, 0, 1].flatten().tolist(),
-                'g0p2': classification_output[:, :, 0, 2].flatten().tolist(),
-                'g0p3': classification_output[:, :, 0, 3].flatten().tolist(),
-                'g1p0': classification_output[:, :, 1, 0].flatten().tolist(),
-                'g1p1': classification_output[:, :, 1, 1].flatten().tolist(),
-                'g1p2': classification_output[:, :, 1, 2].flatten().tolist(),
-                'g1p3': classification_output[:, :, 1, 3].flatten().tolist(),
-                'g2p0': classification_output[:, :, 2, 0].flatten().tolist(),
-                'g2p1': classification_output[:, :, 2, 1].flatten().tolist(),
-                'g2p2': classification_output[:, :, 2, 2].flatten().tolist(),
-                'g2p3': classification_output[:, :, 2, 3].flatten().tolist(),
-                'g3p0': classification_output[:, :, 3, 0].flatten().tolist(),
-                'g3p1': classification_output[:, :, 3, 1].flatten().tolist(),
-                'g3p2': classification_output[:, :, 3, 2].flatten().tolist(),
-                'g3p3': classification_output[:, :, 3, 3].flatten().tolist()
-            })
+            'origin': origins,
+            'lead_time': lead_times,
+            'region': regions,
+            'g0p0': classification_output[:, :, 0, 0].flatten().tolist(),
+            'g0p1': classification_output[:, :, 0, 1].flatten().tolist(),
+            'g0p2': classification_output[:, :, 0, 2].flatten().tolist(),
+            'g0p3': classification_output[:, :, 0, 3].flatten().tolist(),
+            'g1p0': classification_output[:, :, 1, 0].flatten().tolist(),
+            'g1p1': classification_output[:, :, 1, 1].flatten().tolist(),
+            'g1p2': classification_output[:, :, 1, 2].flatten().tolist(),
+            'g1p3': classification_output[:, :, 1, 3].flatten().tolist(),
+            'g2p0': classification_output[:, :, 2, 0].flatten().tolist(),
+            'g2p1': classification_output[:, :, 2, 1].flatten().tolist(),
+            'g2p2': classification_output[:, :, 2, 2].flatten().tolist(),
+            'g2p3': classification_output[:, :, 2, 3].flatten().tolist(),
+            'g3p0': classification_output[:, :, 3, 0].flatten().tolist(),
+            'g3p1': classification_output[:, :, 3, 1].flatten().tolist(),
+            'g3p2': classification_output[:, :, 3, 2].flatten().tolist(),
+            'g3p3': classification_output[:, :, 3, 3].flatten().tolist()
+        })  # NAMGYU: let's shorten this with a for-loop
         self.grid_df = pd.concat([self.grid_df, new_data], ignore_index=True)
 
-    def save_df(self, df: pd.DataFrame, path: str) -> None:
+    def print_regional_metrics(self):
         """
-        Save dataframe to path
-        """
-        df.to_csv(path, index=False)
-
-    def print_raw_data(self, raw_data: pd.DataFrame):
-        """
-        Print regression and classification metrics for raw data (self.regional_df)
-        :param raw_data: pd.DataFrame;  self.regional_df
+        Print regression and classification metrics based on self.regional_df
         """
         # regression
-        target = np.array(raw_data['target_cont'])
-        predictions = np.array(raw_data['predict_cont'])
-        
+        raw_data = self.regional_df
+
+        target = np.array(raw_data['target'])
+        predictions = np.array(raw_data['prediction'])
+
         nmb = np.sum(predictions - target) / np.sum(target)
         nme = np.sum(np.abs(predictions - target)) / np.sum(np.abs(target))
         r, _ = pearsonr(predictions, target)
@@ -139,28 +155,39 @@ class MetricTracker:
         print(f"nmb: {round(nmb, 2)}, nme: {round(nme, 2)}, r: {round(r, 2)}, rmse: {round(rmse, 2)}")
 
         # classification
-        predictions_cate = np.array(raw_data['predict_cate']).reshape(-1, 1).astype(int) # reshape(1, -1)
-        target_cate = np.array(raw_data['target_cate']).reshape(-1, 1).astype(int) # reshape(1, -1)
-        confusion_matrix = calculate_confusion_matrix(predictions_cate, target_cate, n_category=len(self.evaluation_thresholds)+1, device=self.device)
-        
-        self.print_confusion_matrix(confusion_matrix)
+        categorical_predictiones = np.array(raw_data['categorical_prediction']).reshape(-1, 1).astype(
+            int)  # reshape(1, -1)
+        categorical_targetes = np.array(raw_data['categorical_target']).reshape(-1, 1).astype(int)  # reshape(1, -1)
+        confusion_matrix = calculate_confusion_matrix(categorical_predictiones, categorical_targetes,
+                                                      n_classes=len(self.evaluation_thresholds) + 1,
+                                                      device=self.device)
 
-    def print_confusion_matrix(self, cm: np.array, bound:int=2):
+        self.print_categorical_metrics(confusion_matrix)
+
+    def print_categorical_metrics(self, cm: np.array, binary_classification_index: int = 2):
         """
         Print classification metrics for confusion matrix (self.grid_df and self.regional_df)
-        :param cm: np.array of shape (B, n_category, n_category)
+
+        Args:
+            cm: confusion matrix. ndarray of shape (n_samples, n_classes, n_classes)
+        """
+
+        """
+        :param cm:
         """
         # classification
-        g01p01 = np.sum(cm[:, :bound, :bound])
-        g01p23 = np.sum(cm[:, :bound, bound:])
-        g23p01 = np.sum(cm[:, bound:, :bound])
-        g23p23 = np.sum(cm[:, bound:, bound:])
+        bindex = binary_classification_index
+        tn = np.sum(cm[:, :bindex, :bindex])
+        fp = np.sum(cm[:, :bindex, bindex:])
+        fn = np.sum(cm[:, bindex:, :bindex])
+        tp = np.sum(cm[:, bindex:, bindex:])
         total = np.sum(cm)
 
         acc = np.sum(cm[:, 0, 0] + cm[:, 1, 1] + cm[:, 2, 2] + cm[:, 3, 3]) / total
-        hard_acc = np.sum(cm[:, 2, 2] + cm[:, 3, 3]) / (g23p01 + g23p23)
-        far = g01p23 / (g01p23 + g23p23)
-        pod = g23p23 / (g23p23 + g23p01)
+        hard_acc = np.sum(cm[:, 2, 2] + cm[:, 3, 3]) / (fn + tp)
+        far = fp / (fp + tp)
+        pod = tp / (tp + fn)
         f1 = 2 * pod * (1 - far) / (pod + (1 - far))
 
-        print(f"acc: {round(acc, 2)}, hard_acc: {round(hard_acc, 2)}, far: {round(far, 2)}, pod: {round(pod, 2)}, f1: {round(f1, 2)}")
+        print(f"acc: {acc:6.{2}f}, hard_acc: {hard_acc:6.{2}f}, far: 6{far:.{2}f}")
+        print(f"pod: {pod:6.{2}f}, f1: {f1:6.{2}f}")
--- metrics.py
+++ metrics.py
@@ -1,44 +1,49 @@
 import numpy as np
-import pandas as pd
-
-
-import numpy as np
 import torch
 
-def calculate_confusion_matrix(output: np.array, target: np.array, n_category=4, device='cuda'):
+
+def calculate_confusion_matrix(prediction: np.array, target: np.array, n_classes=4, device="cuda") -> np.array:
     """
-    Calculate confusion matrix
-    :param output: np.array of shape (B, N)
-    :param target: np.array of shape (B, N)
-    :param device: 'cpu' or 'cuda' to specify where to run the computation
-    :return: torch.Tensor of shape (B, n_category, n_category)
+    Calculate confusion matrix.
+
+    NAMGYU: TODO - remove device and replace torch with numpy (or convert back to numpy for the final output)
+
+    Args:
+        prediction (np.array): 2d-array of shape (n_samples, n_points_per_sample) with categorical values
+        target (np.array): 2d-array of shape (n_samples, n_points_per_sample) with categorical values
+        n_classes: total number of classes
+        device (str): Device to run the computation on, either "cpu" or "cuda".
+
+    Returns:
+        An array of shape (n_samples, n_classes, n_classes) representing the confusion matrix.
+
+    Example:
         p0 p1 p2 p3
     t0  9  5  1  0
     t1  8  2  3  4
     t2  0  1  2  3
     t3  1  2  3  0
-
     """
+    n_samples, n_points = prediction.shape
 
-    B, N = output.shape
-
-    output = torch.from_numpy(output).to(device)
+    prediction = torch.from_numpy(prediction).to(device)
     target = torch.from_numpy(target).to(device)
 
-    confusion_matrices = torch.zeros((B, n_category, n_category), device=output.device, dtype=torch.int64)
+    confusion_matrices = torch.zeros((n_samples, n_classes, n_classes), device=prediction.device, dtype=torch.int64)
 
     # Create a 2D grid of indices (B*N, 2) indices, where each index is a pair of (target, output)
-    indices = torch.stack([target, output], dim=-1)
+    indices = torch.stack([target, prediction], dim=-1)
     indices = indices.view(-1, 2)
 
     # Convert the 2D indices into linear indices (0, 0) -> 0, (0, 1) -> 1, (0, 2) -> 2, (0, 3) -> 3, (1, 0) -> 4, ...
-    increment_values = torch.ones((B * N,), device=device, dtype=confusion_matrices.dtype)
-    linear_indices = indices[:, 0] * n_category + indices[:, 1]
+    increment_values = confusion_matrices.new_ones(n_samples * n_points)
+    linear_indices = indices[:, 0] * n_classes + indices[:, 1]
 
     # Flatten the output tensor
-    output_tensor_flat = confusion_matrices.view(B, -1)
+    output_tensor_flat = confusion_matrices.view(n_samples, -1)
 
     # Add 1 to the confusion matrix at the linear indices 
-    output_tensor_flat.scatter_add_(1, linear_indices.view(B, N), increment_values.view(B, N))
+    output_tensor_flat.scatter_add_(1, linear_indices.view(n_samples, n_points),
+                                    increment_values.view(n_samples, n_points))
 
-    return confusion_matrices.detach().cpu().numpy()
+    return confusion_matrices.cpu().numpy()
